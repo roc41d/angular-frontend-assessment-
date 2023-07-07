45a2ce7e419a96bfe16d629b7fb7867b9d03410d
Rocard Fonji <fonkengrocard@gmail.com>
2023-07-07 22:18:21 +0100

    (fix): change app name, fix broken test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,9 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-frontend-assessment'`, () => {
+  it(`should have as title 'angular-frontend-for-swapi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-frontend-assessment');
+    expect(app.title).toEqual('angular-frontend-for-swapi');
   });
 });
